5eca8658874300bc67c3985351989440910fe0fa
Haifeng Zhang <haifeng.zhang@adbridgehq.com>
2018-01-16 21:58:40 +0800

    add openvpn-ldap run_docker.sh

--- firewall.rule/run.sh
+++ firewall.rule/run.sh
@@ -1,4 +1,3 @@
 iptables-restore < fw.txt
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' sysctl.conf
 sysctl -p
-system
--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,3 @@
 docker run -d --network=host  --privileged=true henryzhang1/openvpn-ldap
+cd firewall.rule/ && bash run.sh
+
